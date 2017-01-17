0e8552241288b3a00c11d1ceb48c7423bc0ecf7d
Sebastien Deleuze <sdeleuze@pivotal.io>
2017-01-17 17:45:51 +0100

    Use latest spring-boot-web-reactive improvements

--- src/main/java/com/example/functional/WebFunctionPlaygroundApplication.java
+++ src/main/java/com/example/functional/WebFunctionPlaygroundApplication.java
@@ -1,8 +1,6 @@
 package com.example.functional;
 
 
-import java.util.List;
-
 import com.example.functional.domain.User;
 import com.example.functional.domain.UserRepository;
 import reactor.core.publisher.Mono;
@@ -12,34 +10,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.autoconfigure.reactiveweb.ReactiveWebAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.server.reactive.HttpHandler;
-import org.springframework.web.reactive.function.server.RouterFunction;
 
-import static org.springframework.web.reactive.function.server.RouterFunctions.*;
-
-@SpringBootApplication(
-		exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class, ReactiveWebAutoConfiguration.class})
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class WebFunctionPlaygroundApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebFunctionPlaygroundApplication.class, args);
 	}
 
-
 	@Bean
 	public ApplicationRunner databaseInitialization(UserRepository userRepository) {
 		return a -> userRepository.count()
 				.then(n -> n == 0 ? userRepository.save(new User("Stephane", "Nicoll")) : Mono.empty())
 				.block();
-	}
-
-	@Bean
-	public HttpHandler httpHandler(List<RouterFunction> routerFunctions) {
-		return toHttpHandler(
-				routerFunctions.stream().reduce(RouterFunction::and).get()
-		);
 	}
 
 }
